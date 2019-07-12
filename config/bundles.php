3d8ec31b911cc93d21a2b7de13d7f0d77180d716
Renan Taranto <renantaranto@gmail.com>
2019-07-12 02:22:52 -0300

    [Persistence] Add doctrine-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -2,4 +2,6 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
 ];
