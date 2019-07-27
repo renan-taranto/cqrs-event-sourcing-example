24021bf740406b64f546b5643ab5a0e7daf72255
Renan Taranto <renantaranto@gmail.com>
2019-07-27 02:11:27 -0300

    [Validation] Add translator for "ConstraintViolationList"

---
+++ src/Infrastructure/Validation/CommandViolationsTranslator.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * This file is part of list-maker.
+ * (c) Renan Taranto <renantaranto@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Taranto\ListMaker\Infrastructure\Validation;
+
+use Symfony\Component\Validator\ConstraintViolationInterface;
+use Symfony\Component\Validator\ConstraintViolationListInterface;
+
+/**
+ * Class CommandViolationsTranslator
+ * @package Taranto\ListMaker\Infrastructure\Validation
+ * @author Renan Taranto <renantaranto@gmail.com>
+ */
+final class CommandViolationsTranslator implements ConstraintViolationsTranslator
+{
+    /**
+     * @param ConstraintViolationListInterface $violationList
+     * @return array
+     */
+    public function translate(ConstraintViolationListInterface $violationList): array
+    {
+        $errors = [];
+
+        /* @var $violation ConstraintViolationInterface */
+        foreach ($violationList as $violation) {
+            $property = str_replace(['aggregateId', 'payload[', ']'], ['id', '', ''], $violation->getPropertyPath());
+            $errors[$property] = $violation->getMessage();
+        }
+
+        return $errors;
+    }
+}
